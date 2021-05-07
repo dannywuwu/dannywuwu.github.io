4fec694d6ffebee5c0ac8a50e863b13b3f05ed84
dannywuwu <d246wu@gmail.com>
2021-05-07 13:06:30 -0400

    navbox svgs

--- src/Components/Navbox.tsx
+++ src/Components/Navbox.tsx
@@ -1,4 +1,8 @@
 import React from "react";
+import { ReactComponent as ClipboardSvg } from "../res/icons/clipboard.svg";
+import { ReactComponent as GithubSvg } from "../res/icons/github.svg";
+import { ReactComponent as LinkedinSvg } from "../res/icons/linkedin.svg";
+import { ReactComponent as MailSvg } from "../res/icons/mail.svg";
 
 interface NavboxProps {}
 
@@ -7,10 +11,19 @@ export const Navbox: React.FC<NavboxProps> = () => {
     // resume, email, github, linkedin
     <div className="nav-wrapper">
       <div className="nav-icon-row">
-        <a href=""></a>
-        <a href="mailto: d246wu@gmail.com"></a>
-        <a href="https://github.com/dannywuwu"></a>
-        <a href="https://www.linkedin.com/in/wuda/"></a>
+        {/* eslint-disable-next-line */}
+        <a href="">
+          <ClipboardSvg />
+        </a>
+        <a href="mailto: d246wu@gmail.com">
+          <GithubSvg />
+        </a>
+        <a href="https://github.com/dannywuwu">
+          <LinkedinSvg />
+        </a>
+        <a href="https://www.linkedin.com/in/wuda/">
+          <MailSvg />
+        </a>
       </div>
       <ul className="nav-list">
         <li>About</li>
