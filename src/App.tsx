002d8a3a9359c67544db5a3a1e48bf5f1a0a247e
dannywuwu <d246wu@gmail.com>
2021-11-22 23:12:06 -0500

    Grouped introduction into Intro component

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,15 @@
 import React from "react";
 // import logo from './logo.svg';
 import "./res/styles/App.scss";
-import { Navbox } from "./components/Navbox";
 import { About } from "./components/About";
 import { Timeline } from "./components/Timeline";
 import { Contact } from "./components/Contact";
+import { Intro } from "./components/Intro";
 
 function App() {
   return (
     <div className="App">
-      <h1>Danny Wu</h1>
-      <Navbox></Navbox>
+      <Intro></Intro>
       <About></About>
       <Timeline></Timeline>
       <Contact></Contact>
---
+++ src/Components/Intro.tsx
@@ -0,0 +1,13 @@
+import React from "react";
+import { Navbox } from "./Navbox";
+
+interface Props {}
+
+export const Intro = (props: Props) => {
+  return (
+    <div>
+      <h1>Danny Wu</h1>
+      <Navbox></Navbox>
+    </div>
+  );
+};
