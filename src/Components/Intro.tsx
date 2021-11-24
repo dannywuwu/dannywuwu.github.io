61fbe2e8e35b65d49ec0bbd27d634158ae7a80a8
dannywuwu <d246wu@gmail.com>
2021-11-23 23:06:52 -0500

    Intro/Nav CSS

--- src/Components/Intro.tsx
+++ src/Components/Intro.tsx
@@ -1,12 +1,13 @@
 import React from "react";
+import "../res/styles/Intro.scss";
 import { Navbox } from "./Navbox";
 
 interface Props {}
 
 export const Intro = (props: Props) => {
   return (
-    <div>
-      <h1>Danny Wu</h1>
+    <div className="intro-wrapper">
+      <h1 className="intro-name">Danny Wu</h1>
       <Navbox></Navbox>
     </div>
   );
