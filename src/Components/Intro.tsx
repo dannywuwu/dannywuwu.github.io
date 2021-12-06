7e4c657fb03f3ebf0b042e8fa65df1fc1be97331
dannywuwu <d246wu@gmail.com>
2021-12-06 01:07:51 -0500

    center section titles for latop mode

--- src/Components/Intro.tsx
+++ src/Components/Intro.tsx
@@ -7,10 +7,8 @@ interface IntroProps {}
 export const Intro = (props: IntroProps) => {
   return (
     <div className="intro-wrapper">
-      <div className="header-intro">
-        <h1 id="intro-name">Danny Wu</h1>
-        <Navbox></Navbox>
-      </div>
+      <h1 id="intro-name">Danny Wu</h1>
+      <Navbox></Navbox>
     </div>
   );
 };
