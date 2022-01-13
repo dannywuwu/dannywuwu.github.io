afda3113cff0476f3099382b6e7116cda48ca36b
dannywuwu <d246wu@gmail.com>
2022-01-12 21:04:00 -0500

    Updates

---
+++ precache-manifest.b5ba6f8fac1d55cce0d43d835b45a3a5.js
@@ -0,0 +1,54 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "cf56db5f599a664e48285e9f025f6164",
+    "url": "/index.html"
+  },
+  {
+    "revision": "dfae1e7eeb115ce35b7f",
+    "url": "/static/css/main.245a82b9.chunk.css"
+  },
+  {
+    "revision": "7f7506d485d96d436dd5",
+    "url": "/static/js/2.4cbe87e7.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.4cbe87e7.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "dfae1e7eeb115ce35b7f",
+    "url": "/static/js/main.295445d7.chunk.js"
+  },
+  {
+    "revision": "ba126735810163618457",
+    "url": "/static/js/runtime-main.3374ac37.js"
+  },
+  {
+    "revision": "345e30cd8ff7ac05893482ee6d39e672",
+    "url": "/static/media/DannyWuResume.345e30cd.pdf"
+  },
+  {
+    "revision": "c07c916c55ef23e1f0a0dbcb10b9aaae",
+    "url": "/static/media/Karla-Bold.c07c916c.ttf"
+  },
+  {
+    "revision": "1b55fee684d61bfeaa762684931b1bc9",
+    "url": "/static/media/Karla-Regular.1b55fee6.ttf"
+  },
+  {
+    "revision": "1655279a823554231984f35a19327c56",
+    "url": "/static/media/clipboard.1655279a.svg"
+  },
+  {
+    "revision": "ebd86cfc87148a20bb0e3279f489190f",
+    "url": "/static/media/github.ebd86cfc.svg"
+  },
+  {
+    "revision": "849935740434c97f97cf0838e7f0db70",
+    "url": "/static/media/linkedin.84993574.svg"
+  },
+  {
+    "revision": "a96079451be6fae081b0ddd7234db123",
+    "url": "/static/media/mail.a9607945.svg"
+  }
+]);
