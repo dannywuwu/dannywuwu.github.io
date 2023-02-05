c3e26fa0818b3e84e0ea08ec96dada1bddf2f14a
dannywuwu <d246wu@gmail.com>
2023-02-04 23:02:18 -0500

    project card url with updated desktop margins

--- src/Components/ProjectCard.tsx
+++ src/Components/ProjectCard.tsx
@@ -19,9 +19,17 @@ export const ProjectCard = (props: ProjectCardProps) => {
         <h3>{name}</h3>
         <i>{date}</i>
         <p>{desc}</p>
+        <a href={url} className="url" target="_blank" rel="noreferrer noopener">
+          {url}
+        </a>
       </div>
       <div className="project-media">
-        <a href={url} className="img-link">
+        <a
+          href={url}
+          className="img-link"
+          target="_blank"
+          rel="noreferrer noopener"
+        >
           <img src={`images/${imgPath}`} alt={`${name} preview`} />
         </a>
         <p className="tags">Tags: {tags.join(", ")}</p>
