aed62cc636951c3201bb5723f2587d8eebc69c21
dannywuwu <d246wu@gmail.com>
2023-02-21 22:39:34 -0500

    intro visibility animation

--- src/Components/Intro.tsx
+++ src/Components/Intro.tsx
@@ -1,17 +1,29 @@
+import { motion } from "framer-motion";
 import React from "react";
 import "../res/styles/Intro.scss";
 import { Navbox } from "./Navbox";
 
 interface IntroProps {}
 
+const intro = {
+  hidden: { opacity: 0, y: -20 },
+  visible: { opacity: 1, y: 0 },
+};
+
 export const Intro = (props: IntroProps) => {
   return (
-    <div className="intro-wrapper">
+    <motion.div
+      className="intro-wrapper"
+      variants={intro}
+      initial={"hidden"}
+      animate={"visible"}
+      transition={{ duration: 1.5 }}
+    >
       <div className="intro-text">
         <h1 id="intro-name">Danny Wu</h1>
         <p id="intro-education">University of Waterloo SE24</p>
       </div>
       <Navbox></Navbox>
-    </div>
+    </motion.div>
   );
 };
