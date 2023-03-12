a378c4dedfd2f4671f843499bb8987e99c091f54
dannywuwu <d246wu@gmail.com>
2023-03-12 12:53:07 -0400

    refactored profile with HiddenVisible animation

--- src/Components/Profile.tsx
+++ src/Components/Profile.tsx
@@ -1,31 +1,19 @@
-import { motion } from "framer-motion";
 import React from "react";
 import "../res/styles/Profile.scss";
+import HiddenVisibleAnimation from "./HiddenVisibleAnimation";
 
 interface ProfileProps {}
-
-const profile = {
-  hidden: { opacity: 0, y: 50 },
-  visible: { opacity: 1, y: 0 },
-};
 
 export const Profile = (props: ProfileProps) => {
   return (
     <div className="profile-wrapper">
-      <motion.div
-        className="portrait-container"
-        variants={profile}
-        initial={"hidden"}
-        whileInView={"visible"}
-        viewport={{ once: true }}
-        transition={{ duration: 0.5 }}
-      >
+      <HiddenVisibleAnimation duration={0.5}>
         <img src="images/dw.png" alt="danny" id="portrait" />
         <p id="portrait-info">
           Nice to meet you! I am a Software Engineering student (SE 2024) at the
           University of Waterloo, looking for fun programs to play around with.
         </p>
-      </motion.div>
+      </HiddenVisibleAnimation>
     </div>
   );
 };
