261b42f83ef8085a5d36f0608a5cbc99f7ebbf4b
dannywuwu <d246wu@gmail.com>
2023-03-12 12:52:26 -0400

    230312 creatre HiddenVisible animation wrapper component

---
+++ src/Components/HiddenVisibleAnimation.tsx
@@ -0,0 +1,30 @@
+import { motion } from "framer-motion";
+import React, { ReactNode } from "react";
+
+type Props = {
+  children: ReactNode;
+  duration: Number;
+};
+
+const hiddenVisible = {
+  hidden: { opacity: 0, y: 50 },
+  visible: { opacity: 1, y: 0 },
+};
+
+const HiddenVisibleAnimation = (props: Props) => {
+  const { children, duration } = props;
+  return (
+    <motion.div
+      className="portrait-container"
+      variants={hiddenVisible}
+      initial={"hidden"}
+      whileInView={"visible"}
+      viewport={{ once: true }}
+      transition={{ duration }}
+    >
+      {children}
+    </motion.div>
+  );
+};
+
+export default HiddenVisibleAnimation;
